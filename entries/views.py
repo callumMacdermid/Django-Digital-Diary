58c6a2e5c9f245dd0e1d45f4bdbe6c26291cd233
Callum <callum@australiandroid.com.au>
2024-01-26 11:45:34 +1000

    Tutorial finished. Functional diary entry web application. More details to be added soon.

--- entries/views.py
+++ entries/views.py
@@ -1,15 +1,53 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.views.generic import (
     ListView,
-    DetailView
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 
 from .models import Entry
 
-class EntryListView(ListView):
+class LockedView(LoginRequiredMixin):
+    login_url = "admin:login"
+
+class EntryListView(LockedView, ListView):
     model = Entry
     queryset = Entry.objects.all().order_by("-date_created")
 
-class EntryDetailView(DetailView):
+class EntryDetailView(LockedView, DetailView):
     model = Entry
+
+class EntryCreateView(LockedView, SuccessMessageMixin,CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
+    
+class EntryUpdateView(LockedView, SuccessMessageMixin,UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = "Your entry has been updated!"
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "entry-detail",
+            kwargs={"pk": self.object.id}
+        )
+    
+class EntryDeleteView(LockedView, SuccessMessageMixin, DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your entry has been deleted"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
